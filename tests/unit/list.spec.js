4b2cef0a3a2c699cbe8e71365fd349ccfe9d8ec8
Esteban <goldmuros@yahoo.com>
2021-07-12 07:04:09 -0300

    Fixed test error

--- tests/unit/list.spec.js
+++ tests/unit/list.spec.js
@@ -1,16 +1,32 @@
 import { mount, createLocalVue  } from "@vue/test-utils";
 import List from '@/components/activity/List.vue';
 import Element from 'element-ui';
+import Vuex from 'vuex';
 
 const localVue = createLocalVue();
 localVue.use(Element);
 
 describe('List component', () => {
+  let getters;
+  let store;
+
+  beforeEach(() => {
+    getters = {
+      activityList: () => [{
+        id: 1,
+        name: 'todo',
+        completed: false,
+      }]
+    };
+
+    store = new Vuex.Store({
+      getters,
+    })
+  });
+
   it('Exist component', () => {
-    const cmp = mount(List, {localVue });
+    const cmp = mount(List, {store, localVue });
 
     expect(cmp.exists()).toBe(true);
   })
-
-  //TODO make test for edit and removeActivity action
 })
